5dfef1085e88e65d31f98435bfbb821582d21eaf
moodang001 <moo_dang_001@hotmail.com>
2018-02-14 16:38:35 +0700

    Visittor

---
+++ src/main/java/rmuti/ds/App.java
@@ -0,0 +1,33 @@
+package rmuti.ds;
+
+import java.util.ArrayList;
+
+public class App {
+	public static void main(String[] args) {
+		Node f = new Node("f", null, null);
+		Node g = new Node("g", null, null);
+		Node h = new Node("h", null, null);
+		Node i = new Node("i", null, null);
+		
+		Node b = new Node("b", h, i);
+		Node c = new Node("c", f, g);
+		
+		Node root = new Node("a", b, c);
+		
+		final ArrayList lstNode = new ArrayList();
+		Visitor v = new Visitor() {
+			 
+			@Override
+			public void visit(Object e) {
+				lstNode.add(e);
+			} 
+			
+		};
+		TreeTraversal traversal = new TreeTraversal();
+		traversal.postOrder(root, v);
+		System.out.println(lstNode);
+		
+		TreeDraw drawTree = new TreeDraw(root);
+		drawTree.binaryTreeDraw();
+	}
+}
